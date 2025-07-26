50ef24202b1f4d99bed8f1ac371223bcb5b5734e
notes-bin <17797274906@163.com>
2025-07-26 10:31:39 +0800

    fix: delete defaultLogger

--- logger.go
+++ logger.go
@@ -1,7 +1,5 @@
 package cron
 
-import "log/slog"
-
 // Logger 定义了调度器使用的日志接口
 // 允许用户提供自定义日志实现
 // 包含Info和Error两个级别
@@ -18,19 +16,3 @@ func (l *discardLogger) Info(msg string, keysAndValues ...any) {}
 
 // Error 实现Logger接口的Error方法
 func (l *discardLogger) Error(msg string, keysAndValues ...any) {}
-
-// defaultLogger 是默认的日志实现
-// 使用标准库log/slog进行日志输出
-type defaultLogger struct {
-	logger *slog.Logger
-}
-
-// Info 实现Logger接口的Info方法
-func (l *defaultLogger) Info(msg string, keysAndValues ...any) {
-	l.logger.Info(msg, keysAndValues...)
-}
-
-// Error 实现Logger接口的Error方法
-func (l *defaultLogger) Error(msg string, keysAndValues ...any) {
-	l.logger.Error(msg, keysAndValues...)
-}
