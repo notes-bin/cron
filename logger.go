4969e7548733bdc30503aa75884bc9d2a580b2bb
notes-bin <17797274906@163.com>
2025-07-26 09:39:16 +0800

    feat: add log interface

---
+++ logger.go
@@ -0,0 +1,36 @@
+package cron
+
+import "log/slog"
+
+// Logger 定义了调度器使用的日志接口
+// 允许用户提供自定义日志实现
+// 包含Info和Error两个级别
+type Logger interface {
+	// Info 记录信息级别日志
+	// 参数:
+	//   msg - 日志消息
+	//   keysAndValues - 键值对形式的日志参数
+	Info(msg string, keysAndValues ...any)
+
+	// Error 记录错误级别日志
+	// 参数:
+	//   msg - 日志消息
+	//   keysAndValues - 键值对形式的日志参数
+	Error(msg string, keysAndValues ...any)
+}
+
+// defaultLogger 是默认的日志实现
+// 使用标准库log/slog进行日志输出
+type defaultLogger struct {
+	logger *slog.Logger
+}
+
+// Info 实现Logger接口的Info方法
+func (l *defaultLogger) Info(msg string, keysAndValues ...any) {
+	l.logger.Info(msg, keysAndValues...)
+}
+
+// Error 实现Logger接口的Error方法
+func (l *defaultLogger) Error(msg string, keysAndValues ...any) {
+	l.logger.Error(msg, keysAndValues...)
+}
